b489b4b4b9dc4c42efe9f90106cad35f3d5e4186
Julio Punkito <julesnoir@hotmail.fr>
2023-05-09 14:05:59 +0200

    created user model and controller and auth middleware

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const app = express()
+const usersRouter = require("./users");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+	res.render("index", { title: "Express" });
 });
 
+app.use("/users", usersRouter);
+
 module.exports = router;
+
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,125 @@
-var express = require('express');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const auth = require("../middlewares/auth")
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// Créer un nouvel utilisateur
+router.post("/users", async (req, res) => {
+	try {
+		const { name, password } = req.body;
+
+		// Vérifier si l'utilisateur existe déjà
+		const existingUser = await User.findOne({ name });
+		if (existingUser) {
+			return res
+				.status(400)
+				.json({ message: "Cet utilisateur existe déjà" });
+		}
+
+		// Hasher le mot de passe avant de le stocker
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		// Créer un nouvel utilisateur avec le mot de passe hashé
+		const newUser = new User({ name, password: hashedPassword });
+		const savedUser = await newUser.save();
+
+		res.json(savedUser);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "Erreur serveur" });
+	}
 });
 
-module.exports = router;
+// Récupérer tous les utilisateurs
+router.get("/all", auth, async (req, res) => {
+	try {
+		const users = await User.find();
+		res.json(users);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "Erreur serveur" });
+	}
+});
+
+// Récupérer un utilisateur par son ID
+router.get("/users/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const user = await User.findById(id);
+		if (!user) {
+			return res.status(404).json({ message: "Utilisateur non trouvé" });
+		}
+		res.json(user);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "Erreur serveur" });
+	}
+});
+
+// Mettre à jour un utilisateur par son ID
+router.put("/users/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { name, password } = req.body;
+
+		// Vérifier si l'utilisateur existe
+		const existingUser = await User.findById(id);
+		if (!existingUser) {
+			return res.status(404).json({ message: "Utilisateur non trouvé" });
+		}
+
+		// Hasher le nouveau mot de passe avant de le stocker
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		// Mettre à jour l'utilisateur avec le nouveau nom et le nouveau mot de passe hashé
+		existingUser.name = name;
+		existingUser.password = hashedPassword;
+		const updatedUser = await existingUser.save();
+
+		res.json(updatedUser);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ message: "Erreur serveur" });
+	}
+});
+
+// Supprimer un utilisateur par son ID
+router.delete("/users/:id",auth, async (req, res) => {
+	try {
+		const { id } = req.params;
+
+		// Vérifier si l'utilisateur existe
+		const existingUser = await User.findById(id);
+		if (!existingUser) {
+			return res.status(404).json({ message: "Utilisateur non trouvé" });
+		}
+
+		// Supprimer l'utilisateur
+		await existingUser.remove();
+
+		res.json({ message: "Utilisateur supprimé" });
+	} catch (err) {
+		console.error(err);
+		res.status(500);
+	}
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const user = await User.findOne({ name });
+    if (!user) throw Error('User not found');
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw Error('Invalid credentials');
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.status(200).json({ token, user: { id: user._id, name: user.name } });
+  } catch (err) {
+    res.status(400).json({ msg: err.message });
+  }
+});
+
+module.exports = router

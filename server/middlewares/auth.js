685126540ce09cc2f5a11e6993bc4c24b48adfb8
Julio Punkito <julesnoir@hotmail.fr>
2023-05-10 11:47:10 +0200

    added bcrypt decode to jwt middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 // middleware pour vérifier le jeton d'authentification
 const auth = (req, res, next) => {
@@ -6,7 +7,12 @@ const auth = (req, res, next) => {
   if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // Vérification du token hashé avec bcrypt.compareSync()
+    const decodedToken = bcrypt.compareSync(token, process.env.JWT_SECRET);
+    if (!decodedToken) throw new Error();
+
+    // Décodage du token avec jwt.verify()
+    const decoded = jwt.verify(decodedToken, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {

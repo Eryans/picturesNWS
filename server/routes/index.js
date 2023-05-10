a306ed2d56231ef8f65ca14072e1d28849b5cbb1
Julio Punkito <julesnoir@hotmail.fr>
2023-05-10 13:48:02 +0200

    remove useless code

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var router = express.Router();
-const app = express()
+const app = express();
 const usersRouter = require("./users");
+const picturesRouter = require("./userFileManager");
 /* GET home page. */
 router.get("/", function (req, res, next) {
 	res.render("index", { title: "Express" });
 });
 
-app.use("/users", usersRouter);
-
 module.exports = router;
-

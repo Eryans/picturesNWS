affb72bc5c6ff4ac322b32dc966878e02d9e4e30
emmanuel <emmanueldessaint27220@gmail.com>
2023-05-10 15:29:55 +0200

    post image ok

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -6,13 +6,14 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Uploady from "@rpldy/uploady";
 
 export default function Dashboard({ token, setTokenUrl }) {
 
     const [listImg, setListImg] = useState([]);
+    const [uploadedImg, setUploadedImg] = useState({});
 
     const deleteUser = async () => {
-        console.log(localStorage.getItem("tokenExoMai"))
         axios
             .delete(`http://localhost:5000/users/${JSON.parse(localStorage.getItem("infoUserExoMail"))._id}`,
                 {
@@ -31,26 +32,71 @@ export default function Dashboard({ token, setTokenUrl }) {
             });
     }
 
-    // const uploadImg = () = {
+    const uploadImg = (param) => {
+        // console.log(param.target.files[0])
+        setUploadedImg(param.target.files[0])
+    }
 
-    // }
+    const sendImgToServer = () => {
+        // axios
+        //     .post('http://localhost:5000/pictures/upload', {
+        //         uploadedImg
+        //     })
+        //     .then(response => {
+        //         if (response.status === 200) {
+        //             setTokenUrl(response.data.token)
+        //             localStorage.setItem("tokenExoMai", response.data.token);
+        //             localStorage.setItem("infoUserExoMail", JSON.stringify(response.data.newUser));
+        //         }
+        //     });
 
-    useEffect(() => {
-        axios.get('http://localhost:5000/pictures/all', {
-            id: JSON.parse(localStorage.getItem("infoUserExoMail"))._id,
-        })
-            .then(response => {
-                if (response.status === 200) {
-                    console.log(response)
-                    setListImg(response.data)
+    }
+    const handleFileUpload = async (event) => {
+        event.preventDefault()
+        const formData = new FormData();
+        formData.append("file", uploadedImg);
+        console.log(uploadedImg)
+        try {
+            const response = await axios.post("http://localhost:5000/pictures/upload", formData, {
+                headers: {
+                    'auth-token': localStorage.getItem("tokenExoMai"),
                 }
             });
-    }, [])
+            console.log(response.data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    // useEffect(() => {
+    //     axios.get('http://localhost:5000/pictures/all', {
+    //         id: JSON.parse(localStorage.getItem("infoUserExoMail"))._id,
+    //     })
+    //         .then(response => {
+    //             if (response.status === 200) {
+    //                 console.log(response)
+    //                 setListImg(response.data)
+    //             }
+    //         });
+    // }, [])
 
     return (
         <div>
-            <Button variant="contained" color="error" onClick={() => deleteUser()}>Supprimer mon compte</Button>
-            dashboard
+            <div>
+                <Button variant="contained" color="error" style={{ margin: 20 }} onClick={() => deleteUser()}>Supprimer mon compte</Button>
+                <div>
+                    <form encType="multipart/form-data" onSubmit={handleFileUpload}>
+                        <label for="img">Select image:</label>
+                        <input type="file" id="img" name="image" accept="image/*" onChange={(e) => uploadImg(e)} />
+                        <input type="submit" />
+                    </form>
+                </div>
+
+                <div style={{ margin: 20 }}>
+                    {listImg.length === 0 ? "vous n'avez pas encore d'image" :
+                        "test"}
+                </div>
+            </div>
         </div>
     )
 }
